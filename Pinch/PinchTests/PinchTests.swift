77bc4c8f3ce47563c2d419b5200ce95044a0dd7a
Dhruv Patel <dhruvpatel913@gmail.com>
2026-06-18 13:54:07 -0700

    Initial commit

---
+++ Pinch/PinchTests/PinchTests.swift
@@ -0,0 +1,19 @@
+//
+//  PinchTests.swift
+//  PinchTests
+//
+//  Created by Dhruv Patel on 18/06/26.
+//
+
+import Testing
+@testable import Pinch
+
+struct PinchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
