9d6051bb2c27bab848bb5bdceb44da41b54533ea
Dhruv Patel <dhruvpatel913@gmail.com>
2026-06-18 13:54:37 -0700

    Pinch main contentView created.

---
+++ Pinch/Pinch/PinchApp.swift
@@ -0,0 +1,32 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Dhruv Patel on 18/06/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct PinchApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
