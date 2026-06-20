2b7c168c8da49a1469250740bf00b06ca466c720
Dhruv Patel <dhruvpatel913@gmail.com>
2026-06-19 17:47:18 -0700

    Cover image added.

--- Pinch/Pinch/Screen/ContentView.swift
+++ Pinch/Pinch/Screen/ContentView.swift
@@ -18,8 +18,19 @@ struct ContentView: View {
     
     // MARK: - CONTENT
     var body: some View {
-        Text("Hello, World!")
-            .padding()
+        NavigationView {
+            ZStack {
+             // MARK: - PAGE IMAGE
+                Image("magazine-front-cover")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(10)
+                    .padding()
+                    .shadow(color:.black.opacity(0.2),radius: 12,x:2,y:2)
+            }//: ZSTACK
+            .navigationTitle("Pinch & Zoom")
+            .navigationBarTitleDisplayMode(.inline)
+        }//: NAVIGATION
     }
 }
 
