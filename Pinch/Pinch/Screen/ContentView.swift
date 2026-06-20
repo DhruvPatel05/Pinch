11287a2641b264c5c2bf4b91e5c7bd0796c14245
Dhruv Patel <dhruvpatel913@gmail.com>
2026-06-19 17:52:12 -0700

    Animation added.

--- Pinch/Pinch/Screen/ContentView.swift
+++ Pinch/Pinch/Screen/ContentView.swift
@@ -12,6 +12,7 @@ struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
     @Query private var items: [Item]
     // MARK: - PROPERTY
+    @State private var isAnimating: Bool = false
     
     // MARK: - FUNCTION
     
@@ -27,9 +28,15 @@ struct ContentView: View {
                     .cornerRadius(10)
                     .padding()
                     .shadow(color:.black.opacity(0.2),radius: 12,x:2,y:2)
+                    .opacity(isAnimating ? 1:0)
             }//: ZSTACK
             .navigationTitle("Pinch & Zoom")
             .navigationBarTitleDisplayMode(.inline)
+            .onAppear(perform:{
+                withAnimation(.linear(duration: 1)) {
+                    isAnimating = true
+                }
+            })
         }//: NAVIGATION
     }
 }
