601579ddabb592c09d3b0eb94f7a752793089aed
Dhruv Patel <dhruvpatel913@gmail.com>
2026-06-22 15:53:44 -0700

    PageData Model Added.

---
+++ Pinch/Pinch/Model/PageData.swift
@@ -0,0 +1,13 @@
+//
+//  PageData.swift
+//  Pinch
+//
+//  Created by Dhruv Patel on 22/06/26.
+//
+
+import Foundation
+
+let pagesData: [Page] = [
+    Page(id:1, imageName: "magazine-front-cover"),
+    Page(id: 2, imageName: "magazine-back-cover")
+]
