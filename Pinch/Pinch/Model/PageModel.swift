97071882f81258db79f0329c6b45f94d4f22616e
Dhruv Patel <dhruvpatel913@gmail.com>
2026-06-22 15:53:59 -0700

    PageModel Added.

---
+++ Pinch/Pinch/Model/PageModel.swift
@@ -0,0 +1,13 @@
+//
+//  PageModel.swift
+//  Pinch
+//
+//  Created by Dhruv Patel on 22/06/26.
+//
+
+import Foundation
+
+struct Page: Identifiable {
+    let id: Int
+    let imageName: String
+}
